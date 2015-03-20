30560398c39401838e2109625613c25377904351
hollam <rabi.levi@seznam.cz>
2015-03-20 01:04:39 +0100

    first version

---
+++ Weather/SettingViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SettingViewController.swift
+//  Weather
+//
+//  Created by Martin Holla on 14/03/15.
+//  Copyright (c) 2015 Martin Holla. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+
+    @IBOutlet weak var lenghtUnitButton: UIButton!
+    
+    @IBOutlet weak var temperatureUnitButton: UIButton!
+    
+    @IBAction func unitOfLengthClicked(sender: AnyObject) {
+        // changing unit from metres into miles and vice versa
+        StoredData.sharedInstance.unitLengthAreMeters! = !StoredData.sharedInstance.unitLengthAreMeters!
+        setTexts()
+    }
+    
+    @IBAction func unitOfTemperatureClicked(sender: AnyObject) {
+        // changing unit from ceslius into fahrenheit and vice versa
+        StoredData.sharedInstance.unitTemperatureAreCelsius! = !StoredData.sharedInstance.unitTemperatureAreCelsius!
+        setTexts()
+    }
+    
+    func setTexts()
+    {
+        // setting of texts on buttons
+        lenghtUnitButton.setTitle((StoredData.sharedInstance.unitLengthAreMeters! ? "Meters" : "Miles"), forState: UIControlState.Normal)
+        
+        temperatureUnitButton.setTitle(StoredData.sharedInstance.unitTemperatureAreCelsius! ? "Celsius" : "Fahrenheit", forState: UIControlState.Normal)
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // setting of texts on buttons
+        setTexts()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
